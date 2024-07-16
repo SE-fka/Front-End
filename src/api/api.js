749e8a1149a2d49bd33d7f3010d1151099d4bdc3
Fka <test@gmail.com>
2024-07-16 14:29:38 +0300

    first

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 
 //const API_URL = "http://localhost:5000/api/";
-const API_URL = "https://befekadu.onrender.com/api/"
+const API_URL = "https://back-end-6t80.onrender.com/api/"
 
 class ApiService {
   getApi() {
